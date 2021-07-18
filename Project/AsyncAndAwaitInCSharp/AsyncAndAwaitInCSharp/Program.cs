95cbd1b22573710abfc6ac1d42d575b2caada45f
mohamedhashish123 <m.hashish@excprotection.com>
2021-07-18 23:08:49 +0200

    Add project files.

---
+++ Project/AsyncAndAwaitInCSharp/AsyncAndAwaitInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitInCSharp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            kitchen.SynchronousBreakfast();
+            await kitchen.AsynchronousBreakfast();
+            await kitchen.AsynchronousBreakfastBetterWay();
+
+        }
+    }
+}
---
+++ Project/AsyncAndAwaitInCSharp/AsyncAndAwaitInCSharp/Synchronous Breakfast/SyncBreakfast.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwaitInCSharp
+{
+    class SyncBreakfast
+    {
+        public static Toast ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove toast from toaster");
+            return new Toast();
+        }
+        public static Egg FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+            return new Egg();
+        }
+        public static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+        public static void ApplyJam(Toast toast) =>
+           Console.WriteLine("Putting jam on the toast");
+        public static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+    }
+
+}
